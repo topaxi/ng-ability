4e8cd0652f9933515aecd7e70d7edd3021a5de62
Damian Senn <senn@puzzle.ch>
2018-11-07 16:13:42 +0100

    Add missing interface in test app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgAbilityService } from 'ng-ability';
 
 @Component({
@@ -6,7 +6,7 @@ import { NgAbilityService } from 'ng-ability';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-ability-app';
 
   constructor(private readonly ability: NgAbilityService) {}
