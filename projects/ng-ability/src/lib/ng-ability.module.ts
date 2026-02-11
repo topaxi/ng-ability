ec78940563910993a7ab2e5196cad37e6ba43be7
Damian Senn <damian.senn@topaxi.codes>
2026-02-11 14:16:08 +0100

    refactor(module): improve module structure and exports

--- projects/ng-ability/src/lib/ng-ability.module.ts
+++ projects/ng-ability/src/lib/ng-ability.module.ts
@@ -1,31 +1,42 @@
-import { EnvironmentProviders, makeEnvironmentProviders, NgModule } from '@angular/core';
+import {
+  EnvironmentProviders,
+  makeEnvironmentProviders,
+  NgModule,
+  Provider,
+} from '@angular/core';
 import { Ability, AbilityContext } from './interfaces';
 import { ABILITY, ABILITY_CONTEXT } from './ng-ability.service';
 import { CanDirective } from './can.directive';
 import { CanPipe } from './can.pipe';
 
+function abilityToProvider(ability: {
+  new (...args: unknown[]): Ability<unknown, unknown>;
+}): Provider {
+  return { provide: ABILITY, useClass: ability, multi: true };
+}
+
 export function provideAbilities(
-  abilities: { new (...args: unknown[]): Ability<unknown, unknown> }[]
+  abilities: { new (...args: unknown[]): Ability<unknown, unknown> }[],
 ): EnvironmentProviders;
 export function provideAbilities(
   contextClass: { new (...args: unknown[]): AbilityContext<unknown> },
-  abilities: { new (...args: unknown[]): Ability<unknown, unknown> }[]
+  abilities: { new (...args: unknown[]): Ability<unknown, unknown> }[],
 ): EnvironmentProviders;
 export function provideAbilities(
   contextClassOrAbilities:
     | { new (...args: unknown[]): AbilityContext<unknown> }
     | { new (...args: unknown[]): Ability<unknown, unknown> }[],
-  abilities?: { new (...args: unknown[]): Ability<unknown, unknown> }[]
+  abilities?: { new (...args: unknown[]): Ability<unknown, unknown> }[],
 ): EnvironmentProviders {
   if (Array.isArray(contextClassOrAbilities)) {
-    return makeEnvironmentProviders([
-      ...contextClassOrAbilities.map(a => ({ provide: ABILITY, useClass: a, multi: true })),
-    ]);
+    return makeEnvironmentProviders(
+      contextClassOrAbilities.map(abilityToProvider),
+    );
   }
 
   return makeEnvironmentProviders([
     { provide: ABILITY_CONTEXT, useClass: contextClassOrAbilities },
-    ...abilities!.map(a => ({ provide: ABILITY, useClass: a, multi: true })),
+    ...abilities!.map(abilityToProvider),
   ]);
 }
 
@@ -35,24 +46,22 @@ export function provideAbilities(
 })
 export class NgAbilityModule {
   static withAbilities(
-    abilities: { new (...args: unknown[]): Ability<unknown, unknown> }[]
+    abilities: { new (...args: unknown[]): Ability<unknown, unknown> }[],
   ): { ngModule: typeof NgAbilityModule; providers: any[] };
   static withAbilities(
     contextClass: { new (...args: unknown[]): AbilityContext<unknown> },
-    abilities: { new (...args: unknown[]): Ability<unknown, unknown> }[]
+    abilities: { new (...args: unknown[]): Ability<unknown, unknown> }[],
   ): { ngModule: typeof NgAbilityModule; providers: any[] };
   static withAbilities(
     contextClassOrAbilities:
       | { new (...args: unknown[]): AbilityContext<unknown> }
       | { new (...args: unknown[]): Ability<unknown, unknown> }[],
-    abilities?: { new (...args: unknown[]): Ability<unknown, unknown> }[]
+    abilities?: { new (...args: unknown[]): Ability<unknown, unknown> }[],
   ) {
     if (Array.isArray(contextClassOrAbilities)) {
       return {
         ngModule: NgAbilityModule,
-        providers: [
-          ...contextClassOrAbilities.map(a => ({ provide: ABILITY, useClass: a, multi: true })),
-        ],
+        providers: contextClassOrAbilities.map(abilityToProvider),
       };
     }
 
@@ -60,7 +69,7 @@ export class NgAbilityModule {
       ngModule: NgAbilityModule,
       providers: [
         { provide: ABILITY_CONTEXT, useClass: contextClassOrAbilities },
-        ...abilities!.map(a => ({ provide: ABILITY, useClass: a, multi: true })),
+        ...abilities!.map(abilityToProvider),
       ],
     };
   }
